2697365318b8ca4e1d704b08a3e0fee63df39e10
VŨ NGỌC HẢI HÀ <vungochaiha@gmail.com>
2023-04-14 19:09:36 +0700

    navmesh

---
+++ Assets/Scripts/Enemy/EnemyPathfinding.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyPathfinding : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    NavMeshAgent agent;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation= false;
+        agent.updateUpAxis= false;
+    }
+
+    void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+}
